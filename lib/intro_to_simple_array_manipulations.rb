a2d51e0a9d485798558e0ad68ae3b1954f44eb83
Md Abu Talha Mian <sma.talha@yahoo.com>
2020-04-08 03:54:19 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,11 +1,8 @@
-# def using_push(array, string)
-#   array.push(string)
-#   array
-# end
 def using_push(array, string)
   array.push(string)
   array
 end
+
 def using_unshift(array, string)
   array.unshift(string)
 end
